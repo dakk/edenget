cc59d0652bd18924b30bd15ce04912f24048d39b
Davide Gessa <gessadavide@gmail.com>
2012-12-06 22:10:21 +0100

    py2exe

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ import sys
 
 setup_info = dict(
     name = "edenget",
-    version = open("VERSION","r").read()
+    version = open("VERSION","r").read(),
     description = "a multiplatform batch downloader for mangaeden.com",
-    author = emesene.Info.EMESENE_AUTHORS,
+    author = open("AUTHORS","r").read(),
     author_email = "gessadavide@gmail.com",
     keywords = "mangaeden manga batch",
     long_description = """edenget is a multiplatform batch downloader for mangaeden.com""",
-    url = emesene.Info.EMESENE_WEBSITE,
+    url = "http://dakk.github.com/edenget",
     license = "GNU GPL 2",
     classifiers = [
         "License :: OSI Approved :: GNU General Public License (GPL)",
@@ -31,12 +31,15 @@ setup_info = dict(
 )
 
 if os.name == "nt":
-    import py2exe
+	from distutils.core import setup
+	import py2exe
 
-    print "done! files at: dist"
+	setup(console=["main.py"])
+	print "done! files at: dist"
 
 else:
     # Data files to be installed to the system
     _data_files = [    ]
 
     setup(data_files = _data_files, packages = find_packages(), **setup_info)
+    print "done! files at: dist"
